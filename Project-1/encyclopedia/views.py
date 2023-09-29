82daf95aa76f15e937f352b104b5907733dd2665
Sricharan-V <vanguris04@gmail.com>
2023-09-29 11:39:20 +0530

    md to html comverter function

--- Project-1/encyclopedia/views.py
+++ Project-1/encyclopedia/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    md = Markdown()
+    if content == None:
+        return None
+    else:
+        return md.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
